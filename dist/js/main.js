982335bbd1cfd180c02d6a5deded3c0ec7848576
Bushuo <paul.buschmann@chello.at>
2019-01-04 03:27:22 +0100

    add logo styling of project img

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,7 +6,11 @@ const DOMstrings = {
     menuNav: ".menu-nav",
     navItem: ".nav-item",
     message: "#message",
-    subject: "#subject"
+    subject: "#subject",
+    linkHome: "#link-home",
+    linkAbout: "#link-about",
+    linkWork: "#link-work",
+    linkContact: "#link-contact"
 };
 
 let showMenu = false;
@@ -17,10 +21,15 @@ const menuBranding = document.querySelector(DOMstrings.menuBranding);
 const menuNav = document.querySelector(DOMstrings.menuNav);
 const navItems = document.querySelectorAll(DOMstrings.navItem);
 
+const hLink = document.querySelector(DOMstrings.linkHome);
+const aLink = document.querySelector(DOMstrings.linkAbout);
+const wLink = document.querySelector(DOMstrings.linkWork);
+const cLink = document.querySelector(DOMstrings.linkContact);
+
 const message = document.querySelector(DOMstrings.message);
 const subject = document.querySelector(DOMstrings.subject);
 
-menuBtn.addEventListener("click", () => {
+toggleMenu = () => {
     menuBtn.classList.toggle("close");
     menu.classList.toggle("show");
     menuNav.classList.toggle("show");
@@ -31,7 +40,7 @@ menuBtn.addEventListener("click", () => {
 
     // set menu state
     showMenu = !showMenu;
-});
+};
 
 let sendMail = () => {
     message_text = message.value;
@@ -48,3 +57,9 @@ let resetInput = () => {
     message.value = "";
     subject.value = "";
 };
+
+menuBtn.addEventListener("click", toggleMenu);
+hLink.addEventListener("click", toggleMenu);
+aLink.addEventListener("click", toggleMenu);
+wLink.addEventListener("click", toggleMenu);
+cLink.addEventListener("click", toggleMenu);
