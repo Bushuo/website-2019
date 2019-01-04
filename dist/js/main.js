e0255df26faf00427de52edf521f5d4e94b632f8
Bushuo <paul.buschmann@chello.at>
2019-01-04 02:45:09 +0100

    add contact form update content

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,9 @@ const DOMstrings = {
     menu: ".menu",
     menuBranding: ".menu-branding",
     menuNav: ".menu-nav",
-    navItem: ".nav-item"
+    navItem: ".nav-item",
+    message: "#message",
+    subject: "#subject"
 };
 
 let showMenu = false;
@@ -14,6 +16,9 @@ const menu = document.querySelector(DOMstrings.menu);
 const menuBranding = document.querySelector(DOMstrings.menuBranding);
 const menuNav = document.querySelector(DOMstrings.menuNav);
 const navItems = document.querySelectorAll(DOMstrings.navItem);
+
+const message = document.querySelector(DOMstrings.message);
+const subject = document.querySelector(DOMstrings.subject);
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("close");
@@ -27,3 +32,19 @@ menuBtn.addEventListener("click", () => {
     // set menu state
     showMenu = !showMenu;
 });
+
+let sendMail = () => {
+    message_text = message.value;
+    subject_text = subject.value;
+    //console.log(`message = ${message_text}\nsubject = ${subject_text}`);
+    document.location.href =
+        "mailto:me@paulbuschmann.com?subject=" +
+        encodeURIComponent(subject_text) +
+        "&body=" +
+        encodeURIComponent(message_text);
+};
+
+let resetInput = () => {
+    message.value = "";
+    subject.value = "";
+};
